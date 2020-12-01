75b540453fc57c7965f638bfbd7302f73c6be45a
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-12-01 11:12:48 +0530

    Updates

---
+++ precache-manifest.e6e64449e08d9420fc914466c0b82d69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4edac3ee63bb928d824cb4b7fca36a4",
+    "url": "/ZapGiftDemo/index.html"
+  },
+  {
+    "revision": "9876a8a449444d072193",
+    "url": "/ZapGiftDemo/static/css/2.6d972ef2.chunk.css"
+  },
+  {
+    "revision": "b0d33e65789c3f217743",
+    "url": "/ZapGiftDemo/static/css/main.44429b26.chunk.css"
+  },
+  {
+    "revision": "9876a8a449444d072193",
+    "url": "/ZapGiftDemo/static/js/2.36c3552e.chunk.js"
+  },
+  {
+    "revision": "859ae2434a61dfdffa9f7208c890bf34",
+    "url": "/ZapGiftDemo/static/js/2.36c3552e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d33e65789c3f217743",
+    "url": "/ZapGiftDemo/static/js/main.11bb9d4c.chunk.js"
+  },
+  {
+    "revision": "55eefb3b4ac4453b145c",
+    "url": "/ZapGiftDemo/static/js/runtime-main.81b75637.js"
+  }
+]);
